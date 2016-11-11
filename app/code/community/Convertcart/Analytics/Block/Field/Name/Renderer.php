2376ed5fcee6ef0a132d02a5c87c44f58d6034d6
Aamir <aamir@retailon.net>
2016-11-11 11:27:37 +0530

    removed sync code

--- app/code/community/Convertcart/Analytics/Block/Field/Name/Renderer.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Convertcart_Analytics_Block_Field_Name_Renderer extends Mage_Adminhtml_Block_System_Config_Form_Field
-{
-    protected function _getElementHtml($element)
-    {
-        $element->setReadOnly('readonly');
-        return parent::_getElementHtml($element);
-    }
-}
