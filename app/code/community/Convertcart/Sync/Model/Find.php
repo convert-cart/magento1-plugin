4d359c7faa9b4c0f9957cc5b4be9edd1e37f08b1
Aamir <aamir@retailon.net>
2017-05-05 15:45:23 +0530

    added find controller

---
+++ app/code/community/Convertcart/Sync/Model/Find.php
@@ -0,0 +1,149 @@
+<?php
+class Convertcart_Sync_Model_Find extends Mage_Core_Model_Session_Abstract
+{
+    public $debugMode;
+    public $storeId;
+
+    public function getCustomer($params)
+    {
+        if (!isset($params['id']) and !isset($params['email'])) {
+            return null;
+        }
+
+        $this->setParams($params);
+        if (isset($params['id'])) {
+            $customerData = Mage::getModel('customer/customer')->load($params['id'])
+                            ->getData();
+        } elseif (isset($params['email'])) {
+            $websiteId = Mage::getModel('core/store')->load($this->storeId)->getWebsiteId();
+            $customer = Mage::getModel("customer/customer");
+            $customer->setWebsiteId($websiteId);
+            $customer->loadByEmail($params['email']);
+            $customerData = $customer->getData();
+        }
+        if (!is_array($customerData)) {
+            return null;
+        }
+
+        //we dont need hash, dont send these fields
+        unset($customerData['password_hash']);
+        unset($customerData['rp_token']);
+        unset($customerData['rp_token_created_at']);
+        unset($customerData['confirmation']);
+        unset($customerData['disable_auto_group_change']);
+        unset($customerData['reward_update_notificategoryDataion']);
+        unset($customerData['reward_warning_notificategoryDataion']);
+
+        return $customerData;
+    }//getCustomer function ends
+
+    public function getOrder($params)
+    {
+        if (!isset($params['id'])) { //expecting increment_id of order
+            return null;
+        }
+
+        $this->setParams($params);
+        $orderData = Mage::getModel('sales/order_api')->info($params['id']);
+
+        return $orderData;
+    }//getOrder function ends
+
+    public function getProduct($params)
+    {
+        if (!isset($params['id']) and !isset($params['sku'])) {
+            return null;
+        }
+
+        $this->setParams($params);
+        if (isset($params['id'])) {
+            $product = Mage::getModel('catalog/product')
+                        ->load($params['id']);
+        } elseif (isset($params['sku'])) {
+            $product = Mage::getModel('catalog/product')
+                        ->loadByAttribute('sku', $params['sku']);
+        }
+        if (!is_object($product)) {
+            return null;
+        }
+
+        $product->setStoreId($this->storeId);
+
+        $productData = array();
+        $attributes = $product->getAttributes();
+        foreach ($attributes as $attribute) {
+            $attributeCode = $attribute->getAttributeCode();
+            $frontendInput = $attribute->getFrontendInput();
+            if ($frontendInput == 'multiselect' or $frontendInput == 'select') {
+                $productData[$attributeCode] = $product->getAttributeText($attributeCode);
+            } else {
+                $productData[$attributeCode] = $product->getData($attributeCode);
+            }
+        }//foreach attributes ends
+
+        $productData['category_ids'] = $product->getCategoryIds();
+        $stock = Mage::getModel('cataloginventory/stock_item')->loadByProduct($product);
+        $productData['stock_data'] = $stock->getData();
+        $productData['store_url'] = $product->getProductUrl();
+        $productData['url'] = Mage::app()->getStore($this->storeId)->getBaseUrl(Mage_Core_Model_Store::URL_TYPE_DIRECT_LINK).$product->getUrlPath();
+        $productData['image_url'] = Mage::app()->getStore($this->storeId)->getBaseUrl(Mage_Core_Model_Store::URL_TYPE_MEDIA) . 'catalog/product' . $product->getImage();
+        $productData['store_ids'] = $product->getStoreIds();
+
+        return $productData;
+    }//getProduct function ends
+
+    public function getCategory($params)
+    {
+        if (!isset($params['id'])) {
+            return null;
+        }
+
+        $this->setParams($params);
+        $category = Mage::getModel('convertcart_sync/category')
+                    ->load($params['id']);
+
+        if (!is_object($category)) {
+            return null;
+        }
+
+        $categoryData = array();
+        $categoryData['category_id'] = $category->getId();
+        $categoryData['name']        = $category->getData('name');
+        $categoryData['description'] = $category->getData('description');
+        $categoryData['url_key']     = $category->getData('url_key');
+        $categoryData['url']         = Mage::app()->getStore($this->storeId)->getBaseUrl(Mage_Core_Model_Store::URL_TYPE_DIRECT_LINK).$category->getData('url_path');
+        $categoryData['image']       = $category->getData('image');
+
+        $categoryData['meta_title']       = $category->getData('meta_title');
+        $categoryData['meta_keywords']    = $category->getData('meta_keywords');
+        $categoryData['meta_description'] = $category->getData('meta_description');
+
+        $categoryData['is_active']   = $category->getData('is_active');
+        $categoryData['position']    = $category->getData('position');
+        $categoryData['level']       = $category->getData('level');
+        $categoryData['parent_id']   = $category->getData('parent_id');
+        $categoryData['path']        = $category->getData('path');
+        $categoryData['include_in_menu'] = $category->getData('include_in_menu');
+
+        $categoryData['created_at']  = $category->getData('created_at');
+        $categoryData['updated_at']  = $category->getData('updated_at');
+
+        return $categoryData;
+    }// getCategory function ends
+
+    public function debugMode()
+    {
+        if ($this->debug == 1) {
+            error_reporting(E_ALL);
+            ini_set('display_errors', 1);
+            Mage::setIsDeveloperMode(true);
+        }
+    }
+
+    public function setParams($params)
+    {
+        $this->storeId = isset($params['storeId']) ? $params['storeId'] : 0;
+        $this->debug = isset($params['debug']) ? $params['debug'] : 0;
+        $this->debugMode();
+    }
+}
---
+++ app/code/community/Convertcart/Sync/controllers/FindController.php
@@ -0,0 +1,61 @@
+<?php
+class Convertcart_Sync_FindController extends Mage_Core_Controller_Front_Action
+{
+    public function preDispatch()
+    {
+        Mage::helper('convertcart_sync')->authorize();
+        return parent::preDispatch();
+    }
+
+    public function customerAction()
+    {
+        if (Mage::Helper('convertcart_sync')->canSyncCustomer() == false) { //dont proceed if not enabled
+            return;
+        }
+
+        $params = Mage::getModel('convertcart_sync/sync')->getParams();
+        $customerData = Mage::getModel('convertcart_sync/find')->getCustomer($params);
+
+        $this->getResponse()->setHeader('Content-type', 'application/json');
+        $this->getResponse()->setBody(json_encode($customerData));
+    }//customerAction ends
+
+    public function orderAction()
+    {
+        if (Mage::Helper('convertcart_sync')->canSyncOrder() == false) { //dont proceed if not enabled
+            return;
+        }
+
+        $params = Mage::getModel('convertcart_sync/sync')->getParams();
+        $orderData = Mage::getModel('convertcart_sync/find')->getOrder($params);
+
+        $this->getResponse()->setHeader('Content-type', 'application/json');
+        $this->getResponse()->setBody(json_encode($orderData));
+    }//orderAction ends	
+
+    public function catalogAction()
+    {
+        if (Mage::Helper('convertcart_sync')->canSyncCatalog() == false) { //dont proceed if not enabled
+            return;
+        }
+
+        $params = Mage::getModel('convertcart_sync/sync')->getParams();
+        $productData = Mage::getModel('convertcart_sync/find')->getProduct($params);
+
+        $this->getResponse()->setHeader('Content-type', 'application/json');
+        $this->getResponse()->setBody(json_encode($productData));
+    }//catalogAction ends
+
+    public function categoryAction()
+    {
+        if (Mage::Helper('convertcart_sync')->canSyncCatalog() == false) { //dont proceed if not enabled
+            return;
+        }
+
+        $params = Mage::getModel('convertcart_sync/sync')->getParams();
+        $categoryData = Mage::getModel('convertcart_sync/find')->getCategory($params);
+
+        $this->getResponse()->setHeader('Content-type', 'application/json');
+        $this->getResponse()->setBody(json_encode($categoryData));
+    }//categoryAction ends
+}
